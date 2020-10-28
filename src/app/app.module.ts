9f5cc3c5823a9d6d7ab9f7d988c89b4682a6bcf4
thilinamj <thilinamjayawardana@gmail.com>
2020-10-28 19:17:57 +0530

    creare agular project with navbar,about,client,hero,services components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './home/navbar/navbar.component';
+import { HeroSectionComponent } from './home/hero-section/hero-section.component';
+import { AboutSectionComponent } from './home/about-section/about-section.component';
+import { ClientSectionComponent } from './home/client-section/client-section.component';
+import { FeatureSectionComponent } from './home/feature-section/feature-section.component';
+import { StepsSectionComponent } from './home/steps-section/steps-section.component';
+import { ServicesSectionComponent } from './home/services-section/services-section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeroSectionComponent,
+    AboutSectionComponent,
+    ClientSectionComponent,
+    FeatureSectionComponent,
+    StepsSectionComponent,
+    ServicesSectionComponent
   ],
   imports: [
     BrowserModule,
