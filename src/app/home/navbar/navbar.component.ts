09cfd1ef3cc60dd7df10f32685803d33aa696041
thilinamj <thilinamjayawardana@gmail.com>
2020-10-29 12:24:23 +0530

    added sommoth scroll for home, about and services components

--- src/app/home/navbar/navbar.component.ts
+++ src/app/home/navbar/navbar.component.ts
@@ -11,5 +11,13 @@ export class NavbarComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  toHero(){
+    document.getElementById("hero").scrollIntoView({behavior:"smooth"});
+  }
+  toAbout(){
+    document.getElementById("about").scrollIntoView({behavior:"smooth"});
+  }
+  toServices(){
+    document.getElementById("services").scrollIntoView({behavior:"smooth"});
+  }
 }
